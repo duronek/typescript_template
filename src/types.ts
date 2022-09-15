ef7fd1923f4aeb84aba654f5c0b1cf5cc508068e
Artur Nowacki <a.nowacki@aol.com>
2022-09-15 20:00:45 +0200

    Aktualizacja wersji pakietów

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,16 @@
-enum Days {
-	Poniedzialek,
-	Wtorek,
-	Środa,
-	Czwartek,
-	Piątek,
-}
+export const types = () => {
+	enum Days {
+		Poniedzialek,
+		Wtorek,
+		Środa,
+		Czwartek,
+		Piątek,
+	}
 
-const liczba: number | string = 'Artera';
-let text = 'String';
-const myDay: Days = Days.Piątek;
+	const liczba: number | string = 'Artera';
+	let text = 'String';
+	const myDay: Days = Days.Piątek;
 
-text = 'Artur Nowacki';
-console.log(`Dzisiaj jest${myDay}`);
+	text = 'Artur Nowacki';
+	console.log(`Dzisiaj jest${myDay}`);
+};
